5db91e3b62f39a4a83bd729195177a1875b31e10
zongfeizhang <1475377163@qq.com>
2016-10-29 22:17:12 +0800

    对于一个网页的情况
    体会下pr的过程  下面就是循环了   好办  但是可能会面临超时等问题

--- spider/src/main/java/issueRetrieve/spider/IssueRetrive.java
+++ spider/src/main/java/issueRetrieve/spider/IssueRetrive.java
@@ -15,65 +15,58 @@ public class IssueRetrive {
 		Connection conn = Jsoup.connect(issueUrl);
 		Document doc = conn.get();
 		System.out.println(doc.title());
-		///
-		separate();
-		Element ele = doc.getElementById("issuedetails");
-		Elements issueDetails = ele.getElementsByTag("li");
 		
-		//type
-		Element one = issueDetails.get(0);
-		String oneTemp = one.getElementsByTag("div").get(0).getElementById("type-val").text();
-		System.out.println("Type: "+oneTemp);
-		
-		//status
-		Element two = issueDetails.get(1);
-		String twoTemp = two.getElementsByTag("div").get(0).getElementById("status-val").text();
-		System.out.println("status: "+twoTemp);
-		
-		//priority
-		Element three = issueDetails.get(2);
-		String threeTemp = three.getElementsByTag("div").get(0).getElementById("priority-val").text();
-		System.out.println("priority: "+threeTemp);
-		
-		//Resolution
-		Element four = issueDetails.get(3);
-		String fourTemp = four.getElementsByTag("div").get(0).getElementById("resolution-val").text();
-		System.out.println("resolution: "+fourTemp);
-		
-		//Affects Version/s
-		Element five = issueDetails.get(4);
-		String fiveTemp = five.getElementsByTag("div").get(0).getElementById("versions-val").text();
-		System.out.println("Affects Version/s: "+fiveTemp);
-		
-		//Fix Version/s
-		Element six = issueDetails.get(5);
-		String sixTemp = six.getElementsByTag("div").get(0).getElementById("fixfor-val").text();
-		System.out.println("Fix Version/s: "+sixTemp);
-		
-		//Component/s
-		Element seven = issueDetails.get(6);
-		String sevenTemp = seven.getElementsByTag("div").get(0).getElementById("components-val").text();
-		System.out.println("Component/s: "+sevenTemp);
-		
-		//Labels 这个比较特殊  不能直接用id
-		Element eight = issueDetails.get(7);
-		String eightTemp = eight.getElementsByTag("div").get(0).text();
-		System.out.println("Component/s: "+eightTemp);
-		
+		///issueDetails
+		Element issueDetail = doc.getElementById("issuedetails");
+		Elements issueDetails = issueDetail.getElementsByTag("li");
 		System.out.println("-----------------------------------------");
 		for(int i = 0; i < issueDetails.size();i++){
 			Element curElement = issueDetails.get(i);
-			String str = curElement.text();
-			System.out.println(str);
+			String text = curElement.text();
+			System.out.println(text);
 		}
+		
+		System.out.println("-------------------------");
+		//people details
+		Element peopleDetail = doc.getElementById("peopledetails");
+		Elements peopleDetails = peopleDetail.getElementsByTag("li").get(0).getElementsByTag("dl");
+		for(int i = 0; i < peopleDetails.size();i++){
+			Element curElement = peopleDetails.get(i);
+			String text = curElement.text();
+			System.out.println(text);
+		}
+		
+		System.out.println("------------------------");
+		//date details
+		Element dateDetail = doc.getElementById("datesmodule");
+		Elements dateDetails = dateDetail.getElementsByClass("mod-content").get(0).getElementsByTag("ul").
+				get(0).getElementsByTag("li").get(0).getElementsByTag("dl");
+		for(int i = 0; i < dateDetails.size();i++){
+			Element curElement = dateDetails.get(i);
+			String text = curElement.text();
+			System.out.println(text);
+		}
+		
+		System.out.println("------------------------");
+		//description , maybe there are no description, and we will deal with this case later.
+		Element descriptionDetail = doc.getElementById("descriptionmodule");
+		if(descriptionDetail==null){
+			System.out.println("description: null");
+		}
+		else{
+			Elements descriptionDetails = descriptionDetail.getElementsByClass("mod-content");
+			for(int i = 0; i < descriptionDetails.size();i++){
+				Element curElement = descriptionDetails.get(i);
+				String text = curElement.text();
+				System.out.println(text);
+			}
+		}
+		
 	}
 	
-	public void separate(){
-		System.out.println("-------------------------------------------");
-	}
 	
 	public static void main(String[] args) throws IOException{
-		String url = "https://issues.jboss.org/browse/WELD-46";
+		String url = "https://issues.jboss.org/browse/WELD-47";
 		IssueRetrive retrive = new IssueRetrive();
 		retrive.retriveIssueInfo(url);
 		///
